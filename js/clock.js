1635bf37f8e0a5dd4ab64f80b09f1740b46ebf17
Luca Park <qlife1146@gmail.com>
2022-12-24 22:24:21 +0900

    이미지 테스트

--- js/clock.js
+++ js/clock.js
@@ -1,11 +1,10 @@
 const clock = document.querySelector("#clock"); // || h2#clock
 
-const getClock = () => {;
+const getClock = () => {
     const date = new Date();
     let hr = String(date.getHours()).padStart(2, "0");
     let min = String(date.getMinutes()).padStart(2, "0");
     let sec = String(date.getSeconds()).padStart(2, "0");
-
     clock.innerText = `${hr}:${min}:${sec}`;
 };
 
